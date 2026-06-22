336b0c452bc038d1480b8c76e0de995f82ccfc3b
Carlos Silva <mulessiua@gmail.com>
2026-06-22 12:39:17 +0200

    Update BoundariesMOZADM2_2_style.js
    remove default

--- styles/BoundariesMOZADM2_2_style.js
+++ styles/BoundariesMOZADM2_2_style.js
@@ -1,51 +1,82 @@
 var size = 0;
 var placement = 'point';
+
 function categories_BoundariesMOZADM2_2(feature, value, size, resolution, labelText,
                        labelFont, labelFill, bufferColor, bufferWidth,
                        placement) {
-                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
-                switch(valueStr) {default:
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.57}),fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;
-case 'Affected':
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.57}),fill: new ol.style.Fill({color: 'rgba(131,179,224,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;
-case 'Most affected':
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.57}),fill: new ol.style.Fill({color: 'rgba(33,107,178,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;
-default:
-                    return [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.57}),fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-                    break;}};
+
+    var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+
+    switch(valueStr) {
+
+        case 'Affected':
+            return [ new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(35,35,35,1.0)',
+                    lineDash: null,
+                    lineCap: 'butt',
+                    lineJoin: 'miter',
+                    width: 0.57
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(131,179,224,1.0)'
+                }),
+                text: createTextStyle(
+                    feature, resolution, labelText, labelFont,
+                    labelFill, placement, bufferColor, bufferWidth
+                )
+            })];
+            break;
+
+        case 'Most affected':
+            return [ new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(35,35,35,1.0)',
+                    lineDash: null,
+                    lineCap: 'butt',
+                    lineJoin: 'miter',
+                    width: 0.57
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(33,107,178,1.0)'
+                }),
+                text: createTextStyle(
+                    feature, resolution, labelText, labelFont,
+                    labelFill, placement, bufferColor, bufferWidth
+                )
+            })];
+            break;
+
+        default:
+            return [ new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(35,35,35,1.0)',
+                    lineDash: null,
+                    lineCap: 'butt',
+                    lineJoin: 'miter',
+                    width: 0.57
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(255,255,255,1.0)'
+                }),
+                text: createTextStyle(
+                    feature, resolution, labelText, labelFont,
+                    labelFill, placement, bufferColor, bufferWidth
+                )
+            })];
+            break;
+    }
+}
 
 var style_BoundariesMOZADM2_2 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
     };
-    
-    var labelText = ""; 
+
+    var labelText = "";
     var value = feature.get("status");
-    var labelFont = "11.700000000000001px \'Open Sans\', sans-serif";
+    var labelFont = "11.700000000000001px 'Open Sans', sans-serif";
     var labelFill = "#656565";
     var bufferColor = "#fafafa";
     var bufferWidth = 1.5;
@@ -53,13 +84,16 @@ var style_BoundariesMOZADM2_2 = function(feature, resolution){
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
+
     if (feature.get("shapeName") !== null) {
         labelText = String(feature.get("shapeName"));
     }
-    
-    var style = categories_BoundariesMOZADM2_2(feature, value, size, resolution, labelText,
-                            labelFont, labelFill, bufferColor,
-                            bufferWidth, placement);
+
+    var style = categories_BoundariesMOZADM2_2(
+        feature, value, size, resolution, labelText,
+        labelFont, labelFill, bufferColor,
+        bufferWidth, placement
+    );
 
     return style;
 };
